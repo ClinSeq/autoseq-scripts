0937d6e4dee1c76e591c3f5331c58588938d0020
Rebecka Bergström <rebecka.bergstrom@ki.se>
2019-06-27 13:39:08 +0200

    Fix indentation error

--- generateIGVnavInput.py
+++ generateIGVnavInput.py
@@ -97,9 +97,9 @@ src_dir = os.path.abspath(os.path.dirname(os.path.abspath(args.output)))
 
 try:
     if not os.path.exists(igvnav_dirname_dst): os.mkdir(igvnav_dirname_dst)
-        for each_input in [('bams','-nodups.bam'), ('bams','.overlapped.bam'), ('variants','.vep.vcf'), ('svs/igv','.mut'), ('svs','.gtf'),('svs','.bam'),('svs/svaba', '.contigs.bam')]:
-            dir_name = os.path.join(src_dir,each_input[0])
-            create_symlink(dir_name, src_dir, igvnav_dirname_dst)
+    for each_input in [('bams','-nodups.bam'), ('bams','.overlapped.bam'), ('variants','.vep.vcf'), ('svs/igv','.mut'), ('svs','.gtf'),('svs','.bam'),('svs/svaba', '.contigs.bam')]:
+        dir_name = os.path.join(src_dir,each_input[0])
+        create_symlink(dir_name, src_dir, igvnav_dirname_dst)
 except Exception as e:
     print(e)
 
