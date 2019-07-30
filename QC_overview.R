c6d446aa724821fee82362c9a4536a15b9cd8025
Rebecka Bergström <rebecka.bergstrom@ki.se>
2019-07-30 11:10:41 +0200

    Add script that creates plots over QC metrics distributions and status of sample of interest

---
+++ QC_overview.R
@@ -0,0 +1,158 @@
+#!/usr/bin/env Rscript
+
+# This is a script creating plots showing the distribution of some QC values, 
+# and where the current normal and tumor sample lies within those distributions.
+# QC metrics shown are read count, duplication rate, coverage, fold enrichment, on-bait rate, insert size & contamination
+
+
+library(ggplot2)
+library(optparse)
+
+# read in command line options
+option_list <- list(
+  make_option(c("-t", "--tumor"), action = "store", type = "character",
+              default = NULL, help = "Tumor (cfDNA) of interest"),
+  make_option(c("-n", "--normal"), action = "store", type = "character",
+              default = NULL, help = "Normal of interest"),
+  make_option(c("-o", "--outfile"), action = "store", type = "character",
+              default = "QC_overview.pdf", help = "Path to output pdf file"),
+  make_option(c("-m", "--mainpath"), action = "store", type = "character",
+              default = "/nfs/PROBIO/autoseq-output", help = "Path to output pdf file")
+)
+
+opt <- parse_args(OptionParser(option_list = option_list))
+tumor_name = opt$tumor
+normal_name = opt$normal
+outfile = opt$outfile
+main_path = opt$mainpath
+
+
+# find the qc files for all samples
+cat("Find all available qc files...\n")
+hsmetrics_files = dir(Sys.glob(paste0(main_path, "/*/*/qc/picard")), 
+                pattern = "picard-hsmetrics.txt$", recursive = TRUE, full.names = TRUE)
+markduplicates_files = dir(Sys.glob(paste0(main_path, "/*/*/qc/picard")), 
+                           pattern = "markdups-metrics.txt$", recursive = TRUE, full.names = TRUE)
+insertsize_files = dir(Sys.glob(paste0(main_path, "/*/*/qc/picard")), 
+                           pattern = "picard-insertsize.txt$", recursive = TRUE, full.names = TRUE)
+contest_files = dir(Sys.glob(paste0(main_path, "/*/*/contamination")), 
+                    pattern = "contest.txt$", recursive = TRUE, full.names = TRUE)
+
+# remove files from old pipeline (modified before 2019-04-30)
+hsmetrics_files = hsmetrics_files[file.mtime(hsmetrics_files) > as.POSIXct("2019-04-30")]
+markduplicates_files = markduplicates_files[file.mtime(markduplicates_files) > as.POSIXct("2019-04-30")]
+insertsize_files = insertsize_files[file.mtime(insertsize_files) > as.POSIXct("2019-04-30")]
+contest_files = contest_files[file.mtime(contest_files) > as.POSIXct("2019-04-30")]
+
+
+# read in the files
+cat("Read in the files...\n")
+HsMetrics = data.frame()
+for (f in hsmetrics_files) {
+  SAMP = strsplit(basename(f), split = "\\.")[[1]][1]
+  HsMetrics = rbind(HsMetrics, cbind(SAMP, read.table(f, skip = 6, nrow = 1, sep = "\t", 
+                                                        header = TRUE, stringsAsFactors = FALSE), 
+                                     stringsAsFactors = FALSE))
+}
+MarkDuplicates = data.frame()
+for (f in markduplicates_files) {
+  SAMP = sub("-markdups-metrics.txt", "", basename(f))
+  MarkDuplicates = rbind(MarkDuplicates, cbind(SAMP, read.table(f, skip = 6, nrow = 1, sep = "\t", 
+                                                                  header = TRUE, stringsAsFactors = FALSE), 
+                                               stringsAsFactors = FALSE))
+}
+InsertSize = data.frame()
+for (f in insertsize_files) {
+  SAMP = strsplit(basename(f), split = "\\.")[[1]][1]
+  InsertSize = rbind(InsertSize, cbind(SAMP, read.table(f, skip = 6, nrow = 1, sep = "\t", 
+                                                        header = TRUE, stringsAsFactors = FALSE), 
+                                     stringsAsFactors = FALSE))
+}
+ContEst = data.frame()
+for (f in contest_files) {
+  SAMP = strsplit(basename(f), split = "\\.")[[1]][1]
+  ContEst = rbind(ContEst, cbind(SAMP, read.table(f, nrow = 1, sep = "\t", header = TRUE, stringsAsFactors = FALSE), 
+                                       stringsAsFactors = FALSE))
+}
+
+# merge the QC tables and add sample type and capture kit
+qc_merge = merge(merge(merge(HsMetrics, MarkDuplicates, by = "SAMP"), InsertSize, by = "SAMP"), ContEst, by = "SAMP")
+qc_merge$sample_type = sapply(strsplit(qc_merge$SAMP, split = "-"), "[", 4)
+qc_merge$capture = sapply(strsplit(qc_merge$SAMP, split = "-"), "[", 7)
+
+# label the samples of interest (soi)
+qc_merge$soi = qc_merge$SAMP %in% c(tumor_name, normal_name)
+
+
+##################################################################################
+#   ____   __       ____   _______
+#  | o  \ | |     /     \ |__  __|
+#  | __/  | |    |  / \ |   | |
+#  ||     | |___ | \ /  |   | |
+#  ||     |____| \____ /    |_|
+#################################################################################
+
+# Center plot titles (must be specified as of ggplot v 2.2.0)
+theme_update(plot.title = element_text(hjust = 0.5))
+theme_update(plot.subtitle = element_text(hjust = 0.5))
+
+# plotting function to create desired histograms 
+my_histogram = function(x, binwidth, sample_binwidth, ybreaks, xbreaks, xbreaks_minor, x_string, title_string) {
+  p = ggplot(qc_merge, aes_string(x = x)) +
+    geom_histogram(aes(group = capture, fill = capture), binwidth = binwidth, alpha = 0.7, color = "black") +
+    # geom_freqpoly(binwidth = binwidth, alpha = 0.5) +
+    geom_histogram(data = subset(qc_merge, soi), binwidth = sample_binwidth, fill = "red") + # the sample of interest
+    scale_fill_manual(values = c("antiquewhite", "aliceblue")) +
+    scale_y_continuous(breaks = ybreaks) +
+    scale_x_continuous(name = x_string, breaks = xbreaks, minor_breaks = xbreaks_minor) +
+    facet_wrap(~sample_type, ncol = 1) +
+    ggtitle(paste0(title_string, " (binwidth ", binwidth, ")"), 
+            subtitle = paste0("Red piles show present samples (binwidth ", sample_binwidth, ")"))
+  print(p)
+}
+
+# Plot histograms 
+cat(paste0("Create plots (saved in ", outfile, ") ...\n"))
+pdf(file = outfile, width=14)
+
+# read count
+my_histogram(x = "READ_PAIRS_EXAMINED", binwidth = 5e6, sample_binwidth = 1e5, ybreaks = seq(0,nrow(qc_merge), 1),
+             xbreaks = seq(0, 1e12, 1e7), xbreaks_minor = seq(0, 1e12, 5e6), x_string = "number of read pairs", 
+             title_string = "Read count")
+
+# duplication
+my_histogram(x = "PERCENT_DUPLICATION", binwidth = 0.05, sample_binwidth = 0.01, ybreaks = seq(0,nrow(qc_merge), 1),
+             xbreaks = seq(0, 1, 0.1), xbreaks_minor = seq(0, 1, 0.05), x_string = "duplication rate", 
+             title_string = "Duplication rate")
+
+# coverage
+my_histogram(x = "MEAN_TARGET_COVERAGE", binwidth = 100, sample_binwidth = 1, ybreaks = seq(0,nrow(qc_merge), 1),
+             xbreaks = seq(0, 5000, 500), xbreaks_minor = seq(0, 5000, 100), x_string = "mean target coverage", 
+             title_string = "Coverage")
+
+# fold enrichment
+my_histogram(x = "FOLD_ENRICHMENT", binwidth = 50, sample_binwidth = 1, ybreaks = seq(0,nrow(qc_merge), 1),
+             xbreaks = seq(0, 5000, 100), xbreaks_minor = seq(0, 5000, 50), x_string = "fold enrichment", 
+             title_string = "Fold enrichment")
+
+# on-bait rate
+my_histogram(x = "ON_BAIT_BASES/PF_BASES_ALIGNED", binwidth = 0.05, sample_binwidth = 0.01, ybreaks = seq(0,nrow(qc_merge), 1),
+             xbreaks = seq(0, 1, 0.05), xbreaks_minor = seq(0, 1, 0.01), x_string = "dedupped on-bait rate", 
+             title_string = "Dedupped on-bait rate")
+
+# insert size
+my_histogram(x = "MEDIAN_INSERT_SIZE", binwidth = 2, sample_binwidth = 1, ybreaks = seq(0,nrow(qc_merge), 1),
+             xbreaks = seq(0, 1000, 10), xbreaks_minor = seq(0, 1000, 2), x_string = "median insert size", 
+             title_string = "Insert size")
+
+# contamination
+my_histogram(x = "contamination", binwidth = 0.1, sample_binwidth = 0.1, ybreaks = seq(0,nrow(qc_merge), 2),
+             xbreaks = seq(0, 100, 0.5), xbreaks_minor = seq(0, 100, 0.1), x_string = "contamination, %", 
+             title_string = "Contamination")
+
+
+dev.off()
+
+cat("Completed.\n")
+
+
