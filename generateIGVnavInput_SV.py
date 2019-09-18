94a92fc2f832f553e6c1e793bff34bbb3b23b890
Venkatesh Chellappa <genomics@live.com>
2019-09-18 14:18:35 +0200

    Fixed bugs in SV_summary table generation. Fixed problematic headers Added correct annotation bed format

--- generateIGVnavInput_SV.py
+++ generateIGVnavInput_SV.py
@@ -5,18 +5,22 @@ import vcf
 import os
 import shutil
 
+
+
+
 def parse_svaba(input_vcf, SDID, output, vcftype):
     """
     vawk '{print $1, $2, $2+1, "P-00356971_svaba", "BND", $5, S$*$AD, S$*$DP}'
     """
-    header = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_normal\tSUPPORT_tumor\tDPnormal\tDPtumor NOTES\tGENES\"" + " > "+ output +  "_" + vcftype + "_svaba.mut"
+    header = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_normal\tSUPPORT_tumor\tDPnormal\tDPtumor\tGENES\"" + " > "+ output +  "_" + vcftype + "_svaba.mut"
     svaba_cmd = "vawk '{print $1, $2, $2+1, \"" + SDID + "_svaba\", \"BND\", $5, S$*$AD, S$*$DP}' " + \
                 " " + input_vcf + " >> " + output + "_" + vcftype + "_svaba.mut"
 
-    subprocess.call(" && ".join([header, svaba_cmd]), shell=True)
+    cmd = "awk '{print $1, $2, $3, $4, $5," + sv_caller + vcftype + "}' "  + output + "_" + vcftype + "_svaba.mut >> " + output_dir + "/annotate_combined_sv.txt"
+    subprocess.call(" && ".join([header, svaba_cmd, cmd]), shell=True)
 
 
-def parse_svict(input_vcf, SDID, output):
+def parse_svict(input_vcf, SDID, output, vcftype):
     """
     CHROM START   END SDID    SVTYPE  ALT SUPPORTING_READS    NOTES   GENES
     vawk '{if (I$SUPPORT>8 && I$SVTYPE ~ "INS" ) print $1, $2, I$END, "$SDID_svict", I$SVTYPE, "<INS>", I$SUPPORT 
@@ -24,8 +28,8 @@ def parse_svict(input_vcf, SDID, output):
         else if (I$SUPPORT>8 && I$SVTYPE ~ "DEL" ) print $1, $2, I$END, "P-00356971_svict", I$SVTYPE, "<DEL>", I$SUPPORT
         else if (I$SUPPORT>8 && I$SVTYPE ~ "BND" ) print $1, $2, $2+1, "$SDID_svict", I$SVTYPE, $5, I$SUPPORT}' $input.vcf
     """
-    header8 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_READS\tNOTES\tGENES\"" + " > "+ output + "_svict_SR8.mut"
-    header12 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_READS\tNOTES\tGENES\"" + " > "+ output + "_svict_SR12.mut"
+    header8 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_READS\tGENES\"" + " > "+ output + "_svict_SR8.mut"
+    header12 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_READS\tGENES\"" + " > "+ output + "_svict_SR12.mut"
 
     sup_8 = "vawk '{if (I$SUPPORT>8 && I$SVTYPE ~ \"INS\" ) print $1, $2, I$END, \"" + SDID + "_svict\", I$SVTYPE, \"<INS>\", I$SUPPORT ;" + \
         " else if (I$SUPPORT>8 && I$SVTYPE ~ \"INV\" ) print $1, $2, I$END, \"" + SDID + "_svict\", I$SVTYPE, \"<INV>\", I$SUPPORT ; " + \
@@ -38,8 +42,9 @@ def parse_svict(input_vcf, SDID, output):
         " else if (I$SUPPORT>12 && I$SVTYPE ~ \"DEL\" ) print $1, $2, I$END, \"" + SDID + "_svict\", I$SVTYPE, \"<DEL>\", I$SUPPORT ;" + \
         " else if (I$SUPPORT>12 && I$SVTYPE ~ \"BND\" ) print $1, $2, $2+1, \"" + SDID + "_svict\", I$SVTYPE, $5, I$SUPPORT}' " + input_vcf + \
         " >> " + output + "_svict_SR12.mut"
-    
-    subprocess.call(" && ".join([header8, header12, sup_8, sup_12]), shell=True)
+  
+    cmd = "awk '{print $1, $2, $3, $4, $5," + sv_caller + vcftype + "}' "   +output + "_svict_SR8.mut" + ">> " + output_dir + "/annotate_combined_sv.txt"
+    subprocess.call(" && ".join([header8, header12, sup_8, sup_12, cmd]), shell=True)
 
 def parse_lumpy(input_vcf, SDID, output):
     """
@@ -47,27 +52,19 @@ def parse_lumpy(input_vcf, SDID, output):
     vawk '{if ((I$SVLEN>1000 || I$SVLEN<-1000) && $1 != "hs37d5" && $1 !~ "GL" && $5 !~ "hs37d5" && I$SU>50 && I$SVTYPE ~ "BND") print $1, $2, $2+1, "P-00356971_lumpy", I$SVTYPE, $5, "NA", I$SU
          else if ((I$SVLEN>1000 || I$SVLEN<-1000) && $1 != "hs37d5" && $1 !~ "GL" && $5 !~ "hs37d5" && I$SU>50 && I$SVTYPE !~ "BND") print $1, $2, I$END, "P-00356971_lumpy", I$SVTYPE, $5, I$SVLEN, I$SU}'
     """
-    header1k_sup_50 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSVLENGTH\tSUPPORT_READS\tNOTES\tGENES\"" + " > "+ output + "_lumpy_len1k_SU50.mut"
-    header500_sup_24 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSVLENGTH\tSUPPORT_READS\tNOTES\tGENES\"" + " > "+ output + "_lumpy_len500_SU24.mut"
+    header1k_sup_50 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_READS\tGENES\"" + " > "+ output + "_lumpy_len1k_SU50.mut"
+    header500_sup_24 = "echo \"CHROM\tSTART\tEND\tSDID\tSVTYPE\tALT\tSUPPORT_READS\tGENES\"" + " > "+ output + "_lumpy_len500_SU24.mut"
 
     len1k_sup_50 = "vawk '{if ((I$SVLEN>1000 || I$SVLEN<-1000) && $1 != \"hs37d5\" && $1 !~ \"GL\" && $5 !~ \"hs37d5\" && I$SU>50 && I$SVTYPE ~ \"BND\") print $1, $2, $2+1, \""+ SDID +"_lumpy\", I$SVTYPE, $5, \"NA\", I$SU ;"  + \
-                " else if ((I$SVLEN>1000 || I$SVLEN<-1000) && $1 != \"hs37d5\" && $1 !~ \"GL\" && $5 !~ \"hs37d5\" && I$SU>50 && I$SVTYPE !~ \"BND\") print $1, $2, I$END, \""+ SDID +"_lumpy\", I$SVTYPE, $5, I$SVLEN, I$SU}' " + input_vcf + \
+                " else if ((I$SVLEN>1000 || I$SVLEN<-1000) && $1 != \"hs37d5\" && $1 !~ \"GL\" && $5 !~ \"hs37d5\" && I$SU>50 && I$SVTYPE !~ \"BND\") print $1, $2, I$END, \""+ SDID +"_lumpy\", I$SVTYPE, $5, I$SU}' " + input_vcf + \
                 " >> " + output + "_lumpy_len1k_SU50.mut"
 
     len500_sup_24 = "vawk '{if ((I$SVLEN>500 || I$SVLEN<-500) && $1 != \"hs37d5\" && $1 !~ \"GL\" && $5 !~ \"hs37d5\" && I$SU>24 && I$SVTYPE ~ \"BND\") print $1, $2, $2+1, \"" + SDID + "_lumpy\", I$SVTYPE, $5, I$SVLEN, I$SU ;" + \
                 " else if ((I$SVLEN>500 || I$SVLEN<-500) && $1 != \"hs37d5\" && $1 !~ \"GL\" && $5 !~ \"hs37d5\" && I$SU>24 && I$SVTYPE !~ \"BND\") print $1, $2, I$END, \"" + SDID + "_lumpy\", I$SVTYPE, $5, I$SVLEN, I$SU }' " + input_vcf + \
                 " >> " + output + "_lumpy_len500_SU24.mut"
-
-    subprocess.call(" && ".join([header1k_sup_50, header500_sup_24, len1k_sup_50, len500_sup_24]), shell=True)
-
-
-def create_symlink(travers_dir_name, src_dir, igvnav_dirname_dst, suffix):
-    "Recursively Traverse through the directory and create symlink"
-    for root, dirs, files in os.walk(travers_dir_name):
-        for each_file in files:
-            if each_file.endswith(suffix) and not os.path.exists(os.path.join(igvnav_dirname_dst,each_file)):
-                os.symlink(os.path.join(root,each_file), os.path.join(igvnav_dirname_dst,each_file))
-    return
+  
+    cmd = "awk '{print $1, $2, $3, $4, $5," + sv_caller + vcftype + "}' " + output + "_lumpy_len500_SU24.mut" +  ">> " + output_dir + "/annotate_combined_sv.txt"
+    subprocess.call(" && ".join([header1k_sup_50, header500_sup_24, len1k_sup_50, len500_sup_24, cmd]), shell=True)
 
 
 
@@ -82,35 +79,38 @@ parser = argparse.ArgumentParser(description=
     4. sample or patient ID \
     5. mutation type (for example, Synonymous, Missense, Nonsense, Indel, etc.)')
 parser.add_argument('--vcf', required=True, help="Input VCF file")
+parser.add_argument('--annotBed', required=True, help="UCSC hg19 genes bed file with chrom, start, end and genesymbol")
 parser.add_argument('--sdid', required=True, help="SDID from analysis")
 parser.add_argument('--vcftype', help="somatic (or) germline vcf (only for svaba)")
 parser.add_argument('--tool', required=True, help="Tool name - Variant callers")
 parser.add_argument('--output', required=True,
-                    help="output tab demilited file for IGVNav, format=output.mut")
+                    help="output tab delimited file for IGVNav, format=output.mut")
 args = parser.parse_args()
+
 
 vcftype = args.vcftype
 vcf = args.vcf
+annotBed = args.annotBed
 sv_caller = args.tool
 sdid = args.sdid
 output = args.output
 
+output_dir = os.path.dirname(output)
+if not os.path.exists(output_dir + "/annotate_combined_sv.txt"):
+    header2 = "echo \"CHROM\tSTART\tEND\tSVTYPE\tTOOL\tSAMPLE\tGENES" + ">>" + output_dir + "/annotate_combined_sv.txt"
+    subprocess.call(header2, shell=True)
+
+
+
 if sv_caller == 'svict':
-    parse_svict(vcf, sdid, output)
+    parse_svict(vcf, sdid, output, vcftype)
 elif sv_caller == 'lumpy':
     parse_lumpy(vcf, sdid, output)
 elif sv_caller == 'svaba':
     parse_svaba(vcf, sdid, output, vcftype)
+elif sv_caller == 'svcaller':
+    parse_svcaller(gtf, sdid, output)
 
-igvnav_dirname_dst = os.path.join(os.path.dirname(os.path.abspath(args.output)), 'IGVnav')
-src_dir = os.path.abspath(os.path.dirname(os.path.abspath(args.output)))
 
-try:
-    if not os.path.exists(igvnav_dirname_dst): os.mkdir(igvnav_dirname_dst)
-    for each_input in [('bams','-nodups.bam'), ('bams','.overlapped.bam'), ('variants','.vep.vcf'), ('svs/igv','.mut'), ('svs','.gtf'),('svs','.bam'),('svs/svaba', '.contigs.bam')]:
-        dir_name = os.path.join(src_dir,each_input[0])
-        create_symlink(dir_name, src_dir, igvnav_dirname_dst, each_input[1])
-except Exception as e:
-    print(e)
 
 
