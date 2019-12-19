58e877d90f819a60e4ac8c126044a79e99679453
vinaykaikala <vinay.kaikala.ki@gmail.com>
2019-12-19 14:38:21 +0530

    cnv annotation gene removed

--- liqbioCNA_Interactive_plots.R
+++ liqbioCNA_Interactive_plots.R
@@ -1305,14 +1305,13 @@ if (!is.null(bins)) {
     geom_point(aes((bins$cumstart[!ix]+bins$cumend[!ix])/2,bins$log2[!ix]), cex=0.5) +
     geom_segment(
       aes(x = segments$cumstart, y = segments$log2, xend = segments$cumend, yend = segments$log2, 
-          text=sprintf("CNVs:<br>: chr: %s <br> log2: %f <br> start: %f <br> end: %f <br> depth: %f <br> probes: %f <br>genes: %s", 
+          text=sprintf("CNVs:<br>chr: %s <br> log2: %f <br> start: %f <br> end: %f <br> depth: %f <br> probes: %f <br>", 
 		       segments$chromosome, 
 		       segments$log2, 
 		       segments$start, 
 		       segments$end, 
 		       segments$depth,
-		       segments$probes,
-		       segments$gene )
+		       segments$probes)
       ),
       size=0.80, 
       show.legend=F,
